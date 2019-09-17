d29fdd2a9304a1cd74ebb8c6d7de8d67e89d81f6
MFAC <ninafroes@gmail.com>
2019-09-17 15:13:20 +0200

    feat: Fetch data from API and render result

--- src/Components/PokemonSearch.tsx
+++ src/Components/PokemonSearch.tsx
@@ -14,6 +14,7 @@ class PokemonSearch extends Component<User, SearchState> {
   constructor(props: User) {
     super(props);
     this.state = {
+      error: false,
       name: '',
       numberOfAbilities: 0,
       baseExperience: 0,
@@ -27,16 +28,50 @@ class PokemonSearch extends Component<User, SearchState> {
 
   onSearchClick = () => {
     const inputValue = this.pokemonRef.current.value;
+    
+    fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}`)
+      .then(res => {
+        if (res.status !== 200) {
+          this.setState({
+            error: true
+          });
+          return;
+        } 
+        res.json().then(data => {
+          this.setState({
+            error: false,
+            name: data.name,
+            numberOfAbilities: data.abilities.length,
+            baseExperience: data.base_experience,
+            imgUrl: data.sprites.front_default
+          })
+        })
+      })
   }
 
   // Error ts2531: Object is possibly null. (for this.pokemonRef.current.value)
   // Fix error: tsconfig.json - add "strictNullChecks": false
 
   render() {
-    const { name, numberOfPokemons } = this.props;
+    const { userName, numberOfPokemons } = this.props;
+    const { error, name, numberOfAbilities, baseExperience, imgUrl } = this.state;
+
+    let resultMarkup;
+
+    if (error) {
+      resultMarkup = <p>Pokemon not found, try again.</p>;
+    } else {
+      resultMarkup = (
+        <div>
+          <img src={imgUrl} alt={`Picture of ${name}`} />
+          <p>{name} has {numberOfAbilities} abilities and {baseExperience} base experience points.</p>
+        </div>
+      )
+    }
+
     return (
       <div>
-        <p>User {name} {numberOfPokemons ? <span>has {numberOfPokemons} pokemons</span> : <span>has no Pokemons</span>}.</p>
+        <p>User {userName} {numberOfPokemons ? <span>has {numberOfPokemons} pokemons</span> : <span>has no Pokemons</span>}.</p>
         <input type="text" ref={this.pokemonRef} placeholder="Pokemon name..."/>
         <Button onClick={this.onSearchClick}>Search</Button>
       </div>
