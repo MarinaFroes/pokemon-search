097ebf31895058b9e1efb61c5f998ed057fdc1fa
MFAC <ninafroes@gmail.com>
2019-09-17 14:55:41 +0200

    feat: Add state

--- src/Components/PokemonSearch.tsx
+++ src/Components/PokemonSearch.tsx
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
 import User from '../interfaces/User.interface';
+import SearchState from '../interfaces/SearchState.interface';
 import styled from 'styled-components';
 
 const Button = styled.button`
-
+  border: 1px solid blue;
+  border-radius: 3px;
+  background-color: #fff;
+  cursor: pointer;
 `;
 
-class PokemonSearch extends Component<User> {
-
-  pokemonRef = () => {
-
+class PokemonSearch extends Component<User, SearchState> {
+  constructor(props: User) {
+    super(props);
+    this.state = {
+      name: '',
+      numberOfAbilities: 0,
+      baseExperience: 0,
+      imgUrl: ''
+    }
+    this.pokemonRef = React.createRef();
+  
   }
+
+  pokemonRef: React.RefObject<HTMLInputElement>;
 
   onSearchClick = () => {
-
+    const inputValue = this.pokemonRef.current.value;
   }
+
+  // Error ts2531: Object is possibly null. (for this.pokemonRef.current.value)
+  // Fix error: tsconfig.json - add "strictNullChecks": false
 
   render() {
     const { name, numberOfPokemons } = this.props;
