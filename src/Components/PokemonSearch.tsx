e80d83a5dce578db9ba40e4a5b4376b07a2492e6
MFAC <ninafroes@gmail.com>
2019-09-17 14:18:26 +0200

    feat: Add PokemonSearch component

---
+++ src/Components/PokemonSearch.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PokemonSearch extends Component<{ name: string, numberOfPokemons: number }> {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default PokemonSearch;
