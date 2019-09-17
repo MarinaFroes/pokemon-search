42def10db9297bdc9882028b0839cfff4e7f8f6c
MFAC <ninafroes@gmail.com>
2019-09-17 14:33:12 +0200

    feat: Add input text to search for pokemons

--- src/Components/PokemonSearch.tsx
+++ src/Components/PokemonSearch.tsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import User from '../interfaces/User.interface';
+import styled from 'styled-components';
 
-class PokemonSearch extends Component<{ name: string, numberOfPokemons: number }> {
+const Button = styled.button`
+
+`;
+
+class PokemonSearch extends Component<User> {
+
+  pokemonRef = () => {
+
+  }
+
+  onSearchClick = () => {
+
+  }
+
   render() {
+    const { name, numberOfPokemons } = this.props;
     return (
       <div>
-        
+        <p>User {name} {numberOfPokemons ? <span>has {numberOfPokemons} pokemons</span> : <span>has no Pokemons</span>}.</p>
+        <input type="text" ref={this.pokemonRef} placeholder="Pokemon name..."/>
+        <Button onClick={this.onSearchClick}>Search</Button>
       </div>
     )
   }
