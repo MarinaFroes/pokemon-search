ca0977d3174a7fd89be85327fa76932b671f232f
MFAC <ninafroes@gmail.com>
2019-09-17 14:19:16 +0200

    feat: Turn App into class based component and import PokemonSearch

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import PokemonSearch from './Components/PokemonSearch';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <Header />
-      
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <PokemonSearch name="Marina" numberOfPokemons={5} />
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App;
